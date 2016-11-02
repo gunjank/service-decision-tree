498f1a86b4ce75faab5e514a2f779c2822e2c833
Gunjan Kumar <sngunjan@yahoo.com>
2016-11-02 14:40:32 -0400

    added new google service handler

---
+++ src/handlers/googleApiServiceHandler.js
@@ -0,0 +1,28 @@
+'use strict';
+
+
+var request = require('request');
+const settings = require('../config/settings');
+
+let googleApiServiceHandler = {
+
+
+    placeGeocode: function (address, cb) {
+        request({
+            url: settings.googleApiService + "/placeGeocode",
+            method: 'GET',
+            qs: {
+                address: 'citibike ' + address
+            }
+        }, function (error, response, body) {
+            if (error) log.error("Address validation service failed -  " + error);
+            if (response) log.info("address validation successful and response status message is - " + response.statusMessage);
+            cb(response, error);
+        });
+    }
+
+
+
+
+}
+module.exports = googleApiServiceHandler;
